89da2b205cf252fca3eb7b46352ad93e5fddfef2
Murager <jmmukanov@gmail.com>
2023-07-01 14:43:18 +0600

    Initial

---
+++ greetings/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <PageNavbar />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import PageNavbar from "@/components/PageNavbar.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    PageNavbar,
+  },
+};
+</script>
